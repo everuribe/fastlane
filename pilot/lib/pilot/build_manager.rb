26b7a121af0e8bc0029451aa69cb9af42f0b82af
Ever <47759786+everuribe@users.noreply.github.com>
2022-09-09 19:03:07 -0300

    explicitly add parameters

--- pilot/lib/pilot/build_manager.rb
+++ pilot/lib/pilot/build_manager.rb
@@ -397,7 +397,7 @@ module Pilot
       # Otherwise use username and password
       tunes_client = Spaceship::ConnectAPI.client ? Spaceship::ConnectAPI.client.tunes_client : nil
 
-      generic_transporter = FastlaneCore::ItunesTransporter.new(options[:username], nil, false, options[:itc_provider], upload: true, api_key: options[:api_key])
+      generic_transporter = FastlaneCore::ItunesTransporter.new(options[:username], nil, false, options[:itc_provider], nil, upload: true, api_key: options[:api_key])
       return generic_transporter if options[:itc_provider] || tunes_client.nil?
       return generic_transporter unless tunes_client.teams.count > 1
 
@@ -406,7 +406,7 @@ module Pilot
         name = team['name']
         provider_id = generic_transporter.provider_ids[name]
         UI.verbose("Inferred provider id #{provider_id} for team #{name}.")
-        return FastlaneCore::ItunesTransporter.new(options[:username], nil, false, provider_id, upload: true, api_key: options[:api_key])
+        return FastlaneCore::ItunesTransporter.new(options[:username], nil, false, provider_id, nil, upload: true, api_key: options[:api_key])
       rescue => ex
         STDERR.puts(ex.to_s)
         UI.verbose("Couldn't infer a provider short name for team with id #{tunes_client.team_id} automatically: #{ex}. Proceeding without provider short name.")
